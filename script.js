32f9448b04f64e21ceeb8fe7f38898e308e65bdb
IWOG <m662607n@icloud.com>
2023-07-14 01:38:38 +0900

    Mu first commit

--- script.js
+++ script.js
@@ -40,15 +40,44 @@ document.querySelector('.details-button').addEventListener('click', function() {
             detailsContent.style.transform = "scale(1)";
             detailsButton.style.marginTop = "2vmin"; // ボタンの位置を少し下に移動
         }, 50);
-        detailsButton.textContent = "Close";
+        detailsButton.textContent = "✖︎";
     } else {
         detailsContent.style.transform = "scale(0)";
         detailsButton.style.marginTop = "0"; // ボタンの位置を元に戻す
         setTimeout(function() {
             detailsContent.style.display = "none";
         }, 500);
-        detailsButton.textContent = "Open this!!";
+        detailsButton.textContent = "⚪︎";
     }
 });
+
+// 人物の写真がクリックされたときに実行する関数
+function rotateImage() {
+    var card = document.querySelector('.card');
+    card.classList.toggle('is-flipped');
+  }
+  
+  // イベントリスナーを設定
+  document.querySelector('.person-image').addEventListener('click', rotateImage);
+  
+
+// // 人物の写真がクリックされたときに実行する関数
+// function rotateImage() {
+//     var image = document.querySelector('.person-image img');
+//     var text = document.querySelector('.rotate-text');
+//     image.classList.toggle('rotate');
+//     text.classList.toggle('show');
+//   }
+  
+//   // ボタンがクリックされたときに実行する関数
+//   function toggleDetails() {
+//     var content = document.querySelector('.details-content');
+//     content.style.display = content.style.display === 'none' ? 'block' : 'none';
+//     content.classList.toggle('open');
+//   }
+  
+//   // イベントリスナーを設定
+//   document.querySelector('.person-image').addEventListener('click', rotateImage);
+//   document.querySelector('.details-button').addEventListener('click', toggleDetails);  
 
 
